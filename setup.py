50f9ad415dd5e06fc36ffbfc3edebe35d16245ab
Danielle Callan <dmleake92@gmail.com>
2025-04-09 14:33:50 -0400

    stuff in case we want it in PyPi later

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import os
 from setuptools import setup, find_packages
 
+# Read the contents of README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read version from version.txt or define it here
+version = '0.1.0'
+
 setup(
-    name="hyphy_results_toolkit",
-    version="0.1.0",
-    packages=find_packages(),
+    name="hyphy-results-toolkit",  # Use hyphen for PyPI name
+    version=version,
+    packages=find_packages(include=['hyphy_results_toolkit', 'hyphy_results_toolkit.*']),
     install_requires=[
         "numpy>=1.20.0",
         "pandas>=1.3.0",
@@ -14,16 +23,28 @@ setup(
             'hyphy-results=hyphy_results_toolkit.cli:main',
         ],
     },
-    author="Danielle Callan, Hannah Verdonk, Sergei Pond",
-    author_email="spond@temple.edu",  # Primary contact
-    description="Tools for analyzing HyPhy results",
-    long_description=open("README.md").read(),
+    author="Danielle Callan, Hannah Verdonk, Sergei L Kosakovsky Pond",
+    author_email="spond@temple.edu",
+    description="A toolkit for analyzing and summarizing HyPhy evolutionary selection analysis results",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/veg/hyphy-results-toolkit",
+    project_urls={
+        'Bug Reports': 'https://github.com/veg/hyphy-results-toolkit/issues',
+        'Source': 'https://github.com/veg/hyphy-results-toolkit',
+    },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    include_package_data=True,
+    zip_safe=False,
 )
